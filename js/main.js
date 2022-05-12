147c3fa30a417cbebb7deb1c98c28f8b593a679e
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2022-05-12 08:13:58 +0300

    Created HTML, started with some simple css, and working through the JS

---
+++ js/main.js
@@ -0,0 +1,77 @@
+// VARIABLE DECLARATION
+
+let employes = [];
+let urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`;
+let gridContainer = document.querySelector(".grid-container");
+let overlay = document.querySelector(".overlay");
+let modalContainer = document.querySelector(".modal-container");
+let modalClose = document.querySelector(".modal-close");
+
+// FETCH FUNCTION TO GRAB API DATA
+fetch(urlAPI)
+  .then((res) => res.json())
+  .then((res) => res.results)
+  .then(displayEmployees)
+  .catch((err) => console.log(err));
+console.log(employes);
+
+// FUNCTION TO DISPLAY EMPLOYEES
+function displayEmployees(employeeData) {
+  // EMPLOYEE HTML
+  let employeeHTML = "";
+
+  // LOOP THROUGH EMPLOYEE DATA AND CREATE HTML
+  displayEmployees.forEach((employee, index) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
+
+    // CREATE HTML
+    employeeHTML += `
+        <div class="card" data-index="${index}">
+            <img class="avatar" src="${picture.large}"/>
+            <div clas="text-container">
+                <h2 class="name">${name.first} ${name.last}</h2>
+                <p class="email">${email}</p>
+                <p class="city">${city}</p>
+            </div>
+        </div>
+        `;
+  });
+  gridContainer.innerHTML = employeeHTML;
+}
+
+// FUNCTION TO DISPLAY MODAL
+function displayModal(index) {
+  // OBJECT DESTRUCTURING
+  let {
+    name,
+    dob,
+    phone,
+    email,
+    location: { city, street, state, postcode },
+    picture,
+  } = employes[index];
+
+  //   GRAB THE BIRTHDATE
+  let date = new Date(dob.date);
+
+  //   CREATE THE MODAL HTML
+  const modalHTML = `
+  <img class="avatar" src="${picture.large}"/>
+  <div class="text-container">
+        <h2 class="name">${name.first} ${name.last}</h2>
+        <p class="email">${email}</p>
+        <p class="address">${city}</p>
+        <hr />
+        <p>${phone}</p>
+        <p class="address">${street}, ${state}, ${postcode}</p>
+        <p>Birtday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div>
+  `;
+  // REMOVE THE HIDDEN CLASS AND ASSIGN THE HTML TO THE MODAL
+  overlay.classList.remove("hidden");
+  modalContainer.innerHTML = modalHTML;
+}
